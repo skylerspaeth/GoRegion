26f4def47b158d3c0ddd93f130fca8c6ae9aa5cb
Skyler Spaeth <skylerspaeth.scc@me.com>
2021-11-13 21:30:04 -0600

    make code better (and more pythonic) in a lot of ways: better variable names, invert if/else statement for better readability, more appropriate error handling, remove unnecessary lambdas, and more

--- goregion.py
+++ goregion.py
@@ -7,40 +7,44 @@ if (sys.version_info[0] < 3):
 
 #JSON manipulation and requests module imports
 import json
-from urllib.request import urlopen
+import urllib.request
+import itertools
 
 #needed for iptables invocation
 import os
 
 #Valve physical network diagram path
-networkDiagramPath = "airports.json"
+NETWORK_DIAGRAM_PATH = "layout.json"
 
-#iptables interface function
+#iptables interface functions
+def callIpTables(args):
+    return os.system("sudo iptables {}".format(args))
+
 def iptc(operation, addresses):
 
     if (operation == "setupChain"):
         print("Creating iptables chain \"goregion\"...")
-        tableCreateCode = 0 if chainPresent else os.system("sudo iptables -N goregion")
-        tableBindCode = 0 if chainBound else os.system("sudo iptables -A INPUT -j goregion")
+        tableCreateCode = 0 if chainPresent else callIpTables("-N goregion")
+        tableBindCode = 0 if chainBound else callIpTables("-A INPUT -j goregion")
         print("Successfully created chain." if tableCreateCode == 0 and tableBindCode == 0 else "Error creating chain.")
 
     elif (operation == "block"):
         print("Blocking the following addresses not contained within selected region:", ", ".join(addresses))
         for address in addresses:
-            os.system("sudo iptables -A goregion -s {} -j DROP".format(address))
+            callIpTables("-A goregion -s {} -j DROP".format(address))
 
     elif (operation == "allow"):
         print("Unblocking the following addresses:", ", ".join(addresses))
         for address in addresses:
-            os.system("sudo iptables -D goregion -s {} -j DROP".format(address))
+            callIpTables("-D goregion -s {} -j DROP".format(address))
 
     elif (operation == "reset"):
         print("Resetting all goregion rules...")
-        print("Successfully reset chain." if os.system("sudo iptables -F goregion") == 0 else "Error resetting chain.")
+        print("Successfully reset chain." if callIpTables("-F goregion") == 0 else "Error resetting chain.")
 
 #check if iptables chain exists and create it if not
-chainPresent = True if os.system("sudo iptables -S | grep -Fx -- '-N goregion'") == 0 else False
-chainBound = True if os.system("sudo iptables -S | grep -Fx -- '-A INPUT -j goregion'") == 0 else False
+chainPresent = True if callIpTables("-S | grep -Fx -- '-N goregion'") == 0 else False
+chainBound = True if callIpTables("-S | grep -Fx -- '-A INPUT -j goregion'") == 0 else False
 chainSetup = True if chainPresent and chainBound else False
 if not chainSetup:
     print("Chain not setup, triggered by values chainPresent: {} and chainBound: {}.".format(chainPresent, chainBound))
@@ -48,48 +52,40 @@ if not chainSetup:
 
 iptc("reset", None)
 
-with urlopen("https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json") as jsonResponse:
-    data = json.loads(jsonResponse.read())
-    allRelays = data["pops"]
-    print("Retrieved SteamDB NetworkDatagramConfig.json version {}.".format(data.get("revision")))
+with urllib.request.urlopen("https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json") as rawData:
+    jsonData = json.loads(rawData.read())
+    allPops = jsonData["pops"]
+    print("Retrieved SteamDB NetworkDatagramConfig.json version {}.".format(jsonData.get("revision")))
 
 
-    with open(networkDiagramPath, "r") as networkDiagramData:
+    with open(NETWORK_DIAGRAM_PATH, "r") as networkDiagramData:
         networkDiagramJSON = json.loads(networkDiagramData.read())
         print("The latest available region codes are as follows:")
-        validRegions = list(map(lambda x: x, networkDiagramJSON.get("regions").keys()))
+        validRegions = networkDiagramJSON.get("regions").keys()
         validRegionsStr = ", ".join(validRegions).upper()
         print(validRegionsStr)
 
         selectedRegion = input("Select a region by entering its code: ").lower()
-        if selectedRegion in validRegions:
-            regionalCities = list(map(lambda x: x, networkDiagramJSON.get("regions").get(selectedRegion).get("cities")))
-            citiesToDisplay = ", ".join(list(map(lambda x: "{} ({})".format(x.get("cityName"), x.get("airportCode").upper()), regionalCities)))
+        if selectedRegion not in validRegions:
+            print("You must enter a valid region from the following:", validRegionsStr)
+        else:
+            regionalCities = networkDiagramJSON["regions"][selectedRegion]["cities"]
+            citiesToDisplay = ", ".join([
+                "{} ({})".format(city.get("cityName"), city.get("airportCode").upper())
+                for city in regionalCities
+            ])
             print("Cities available in the {} region: {}".format(selectedRegion.upper(), citiesToDisplay))
 
-            valveRelayNames = list(allRelays.keys())
-            valveRelays = list(allRelays.values())
+            popsInRegion = list(itertools.chain(*[city.get("referrers", [city.get("airportCode")]) for city in regionalCities]))
+            print("POPs in region:", popsInRegion)
 
-            def callback(currentRelay):
-                if currentRelay.get("referrers"):
-                    return currentRelay["referrers"]
-                else:
-                    return currentRelay["airportCode"]
-
-            relaysInRegion = list(map(callback, networkDiagramJSON["regions"][selectedRegion]["cities"]))
-            print(relaysInRegion)
-            #should this be an array instead of list?
             blacklistAddresses = []
-
-            for (relayName, relay) in zip(valveRelayNames, valveRelays):
-                if relay.get("relays"):
-                    if relayName not in relaysInRegion:
-                        for address in map(lambda x: x["ipv4"], relay["relays"]):
+            for (popName, pop) in allPops.items():
+                if pop.get("relays"):
+                    if popName not in popsInRegion:
+                        for address in [relay["ipv4"] for relay in pop["relays"]]:
                             blacklistAddresses.append(address)
 
             iptc("block", blacklistAddresses)
-
-        else:
-            print("You must enter a valid region from the following:", validRegionsStr)
 
 print("Thank you for using CS:GO region selector.")
